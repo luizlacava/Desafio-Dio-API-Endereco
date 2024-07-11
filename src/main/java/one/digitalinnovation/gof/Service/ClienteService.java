02b269b525885b253a4f45498cd221e2ff80bfc2
luizlacava <lglacava@gmail.com>
2024-07-11 14:57:24 -0300

    primeiro commit desafio API

---
+++ src/main/java/one/digitalinnovation/gof/Service/ClienteService.java
@@ -0,0 +1,18 @@
+package one.digitalinnovation.gof.Service;
+
+import one.digitalinnovation.gof.Model.Cliente;
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+
+}
---
+++ src/main/java/one/digitalinnovation/gof/Service/ViaCepService.java
@@ -0,0 +1,17 @@
+package one.digitalinnovation.gof.Service;
+
+import one.digitalinnovation.gof.Model.Endereco;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepService {
+
+    //@RequestMapping(method = RequestMethod.GET, value = "/{cep}/json/")
+
+    @GetMapping("/{cep}/json/")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+}
